661e4ffda0e3fb5610174ee8e5eeab114d5fb669
vernisaz <javaarchitect@msn.com>
2025-12-09 21:35:04 -0800

    more color work

--- src/color.rs
+++ src/color.rs
@@ -1,42 +1,75 @@
 use std::fmt;
-#[derive(Default, Debug)] 
-enum Color {
+#[derive(Default, Debug, Clone)] 
+pub enum Color {
     Red,
     Yellow,
     Blue,
     Green,
-    Brown,
+    Magenta,
+    Cyan,
+    Black,
+    White,
     #[default]
     System,
 }
-struct ColorHolder{///<'a> {
+#[derive(Default, Debug, Clone)] 
+pub struct ColorHolder{///<'a> {
     fg: Color,
     bg: Color,
     bright: bool,
-    value: String,//Box<dyn std::fmt::Display>, //&'a impl std::fmt::Display,
+    bright_bg: bool,
+    bold: bool,
+    italic: bool,
+    value: String,
 }
 pub trait Colorized : std::fmt::Display {
-    fn green(&self) -> Box<dyn std::fmt::Display + '_> {
-        Box::new(ColorHolder{fg:Color::Green,bg:Color::default(),bright:false,value:self.to_string()})
+   
+    fn blue(&self) -> ColorHolder {
+          ColorHolder{fg:Color::Blue,bg:Color::default(),value:self.to_string(),..Default::default()}
     }
-    fn blue(&self) -> Box<dyn std::fmt::Display + '_> {
-          Box::new(ColorHolder{fg:Color::Blue,bg:Color::default(),bright:false,value:self.to_string()})
+    fn bright(&self) -> ColorHolder {
+          ColorHolder{bright:true,value:self.to_string(),..Default::default()}
     }
-    fn red(&self) -> Box<dyn std::fmt::Display + '_> {
-        Box::new(ColorHolder{fg:Color::Red,bg:Color::default(),bright:false,value:self.to_string()})
+    fn on_black(&self) -> ColorHolder {
+          ColorHolder{bg:Color::Black,value:self.to_string(),..Default::default()}
     }
-
+}
+impl Colorized for ColorHolder {
+    fn blue(&self) -> ColorHolder {
+        let mut res = self.clone();
+        res.fg = Color::Blue;
+        res
+    }
+    fn on_black(&self) -> ColorHolder {
+        let mut res = self.clone();
+        res.bg = Color::Black;
+        res
+    }
+    fn bright(&self) -> ColorHolder {
+        let mut res = self.clone();
+        res.bright = true;
+        res
+    }
 }
 impl fmt::Display for ColorHolder {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut color = String::new();
         match self.fg {
-            Color::Green => write!(f, "\x1b[32m{}\x1b[0m", self.value),
-            Color::Blue => write!(f, "\x1b[34m{}\x1b[0m", self.value),
-            Color::Red => write!(f, "\x1b[31m{}\x1b[0m", self.value),
-
-            _ => write!(f, "{}", self.value)
+            Color::Green => color.push_str("32;"),
+            Color::Blue => if self.bright {color.push_str("94;")} else {color.push_str("34;")},
+            _ => todo!("impl soon")
         }
-        
+        match self.bg {
+            Color::Green => color.push_str("42;"),
+            Color::Blue => color.push_str("44;"),
+            Color::Black => color.push_str("40;"),
+            _ => todo!("impl soon")
+        }
+        if color.is_empty() {
+            write!(f, "{}", self.value)
+        } else {
+            write!(f, "\x1b[{color}m{}\x1b[0m", self.value)
+        }
     }
 }
 impl Colorized for str {}
