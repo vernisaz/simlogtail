74321360cc1881fe0b37dacc2777fe60ca5f0dad
D R <metricstream@gmail.com>
2025-12-10 13:19:04 -0800

    used a new approach

--- src/color.rs
+++ src/color.rs
@@ -1,77 +1,136 @@
 use std::fmt;
-#[derive(Default, Debug, Clone)] 
+#[derive(Default, Debug, Clone, PartialEq)] 
 pub enum Color {
+    Black,
     Red,
+    Green,
     Yellow,
     Blue,
-    Green,
     Magenta,
     Cyan,
-    Black,
     White,
     #[default]
+    Notset,
     System,
 }
-#[derive(Default, Debug, Clone)] 
-pub struct ColorHolder{///<'a> {
+#[derive(Debug, Clone)] 
+pub struct ColorHolder<B>{
+    inner: B,
     fg: Color,
     bg: Color,
     bright: bool,
     bright_bg: bool,
     bold: bool,
     italic: bool,
-    value: String,
+    blink: bool,
+    underline: bool,
+    hidden: bool,
 }
-pub trait Colorized : std::fmt::Display {
-   
-    fn blue(&self) -> ColorHolder {
-          ColorHolder{fg:Color::Blue,bg:Color::default(),value:self.to_string(),..Default::default()}
+pub trait Colorized : Sized {
+    fn color(self, fg: Color) -> ColorHolder<Self> {
+        ColorHolder {
+            inner: self,
+            fg,
+            bg: Default::default(),
+            bright: Default::default(),
+            bright_bg: Default::default(),
+            bold: Default::default(),
+            italic: Default::default(),
+            blink: Default::default(),
+            underline: Default::default(),
+            hidden: Default::default(),
+        }
     }
-    fn bright(&self) -> ColorHolder {
-          ColorHolder{bright:true,value:self.to_string(),..Default::default()}
+    fn on(self) -> ColorHolder<Self> {
+        self.color(Color::System)
     }
-    fn on_black(&self) -> ColorHolder {
-          ColorHolder{bg:Color::Black,value:self.to_string(),..Default::default()}
+    fn blue(self) -> ColorHolder<Self> {
+          self.color(Color::Blue)
+    }
+    fn black(self) -> ColorHolder<Self> {
+        self.color(Color::Black)
+    }
+    fn yellow(self) -> ColorHolder<Self> {
+          self.color(Color::Yellow)
+    }
+    fn red(self) -> ColorHolder<Self> {
+          self.color(Color::Red)
     }
 }
-impl Colorized for ColorHolder {
-    fn blue(&self) -> ColorHolder {
-        let mut res = self.clone();
-        res.fg = Color::Blue;
-        res
+
+impl<T> ColorHolder<T> {
+    fn color(mut self, color: Color) -> Self {
+        if self.fg != Color::Notset {
+            self.bg = color
+        } else {
+            self.fg = color
+        }
+        self
     }
-    fn on_black(&self) -> ColorHolder {
-        let mut res = self.clone();
-        res.bg = Color::Black;
-        res
+    
+    pub fn blue(self) -> Self {
+        self.color(Color::Blue)
     }
-    fn bright(&self) -> ColorHolder {
-        let mut res = self.clone();
-        res.bright = true;
-        res
+    pub fn black(self) -> Self {
+        self.color(Color::Black)
     }
-}
-impl fmt::Display for ColorHolder {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    pub fn yellow(self) -> Self {
+        self.color(Color::Yellow)
+    }
+    pub fn bright(mut self) -> Self {
+        self.bright = true;
+        self
+    }
+    pub fn on(mut self) -> Self {
+        if self.fg == Color::Notset {
+            self.fg = Color::System
+        }
+        self
+    }
+
+    fn ansi(&self) -> String {
         let mut color = String::new();
-        match self.fg {
-            Color::Green => color.push_str("32;"),
-            Color::Blue => if self.bright {color.push_str("94;")} else {color.push_str("34;")},
-            _ => todo!("impl soon")
+        if self.fg != Color::Notset && self.fg != Color::System {
+            if self.bright {color.push('9')} else {color.push('3')} 
+            color .push(get_color_num(&self.fg))
         }
         match self.bg {
             Color::Green => color.push_str("42;"),
             Color::Blue => color.push_str("44;"),
             Color::Black => color.push_str("40;"),
+            Color::Yellow => color.push_str("43;"),
             _ => todo!("impl soon")
         }
+        color
+    }
+}
+fn get_color_num(color: &Color) -> char {
+    match color {
+        Color::Black => '0',
+        Color::Red => '1',
+        Color::Green  => '2',
+        Color::Yellow => '3',
+        Color::Blue => '4',
+        Color::Magenta => '5',
+        Color::Cyan => '6',
+        Color::White => '7',
+        _ => '0'
+    }
+}
+impl<C> fmt::Display for ColorHolder<C> 
+where
+    C: fmt::Display,
+{
+    //fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let color = self.ansi();
         if color.is_empty() {
-            write!(f, "{}", self.value)
+            write!(f, "{}", self.inner)
         } else {
-            write!(f, "\x1b[{color}m{}\x1b[0m", self.value)
+            write!(f, "\x1b[{color}m{}\x1b[0m", self.inner)
         }
     }
 }
-impl Colorized for str {}
+impl Colorized for &str {}
 impl Colorized for String {}
   
--- src/tail.rs
+++ src/tail.rs
@@ -89,7 +89,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                         .and_then(|(date,tail)| {
                             match date.parse::<i64>() {
                                 Ok(date) => { let (y,m,d,h,mm,s,_) = simtime::get_datetime(1970, (date/1000i64 + (tz_off as i64) *60i64) as u64);
-                                    Some(format!("{before} {} {tail}", format!("{m}-{d:02}-{y} {h}:{mm:02}:{s:02}").blue().bright().on_black()))},
+                                    Some(format!("{before} {} {tail}", format!("{m}-{d:02}-{y} {h}:{mm:02}:{s:02}").blue().bright().on().yellow()))},
                                 _ => None,
                             } }) }) {
                                 Some(line) => println!("{}", line),
