beb754e2dd8c1c8e74d28496dfe04a6ce98d5d9d
V <javaarchitect@msn.com>
2026-02-01 21:15:11 -0800

    cli formatted

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use std::{env, collections::HashSet, fmt};
+use std::{collections::HashSet, env, fmt};
 
 #[cfg(unix)]
 const OPT_PREFIX: char = '-';
@@ -11,18 +11,18 @@ pub enum OptTyp {
     FNum,
     Str,
     InStr,
-    None
+    None,
 }
 #[derive(PartialEq, Debug)]
 pub enum OptVal {
     Num(i64),
     FNum(f64),
     Str(String),
-    Arr(HashSet<(String,String)>),
+    Arr(HashSet<(String, String)>),
     Empty,
-    Unmatch
+    Unmatch,
 }
-#[derive(Debug)] 
+#[derive(Debug)]
 pub struct OptError {
     cause: String,
 }
@@ -42,7 +42,7 @@ pub struct CLI {
     args: Vec<String>,
     opts: Vec<CliOpt>,
     descr: Option<String>,
-    unprocessed: bool
+    unprocessed: bool,
 }
 impl CLI {
     pub fn new() -> Self {
@@ -50,31 +50,40 @@ impl CLI {
             args: vec![],
             opts: vec![],
             descr: None,
-            unprocessed: true
+            unprocessed: true,
         }
     }
-    
+
     pub fn opt(&mut self, name: &str, t: OptTyp) -> Result<&mut Self, OptError> {
         if !self.unprocessed {
-            return Err(OptError{cause: format!("the option {name} can't be set after parsing arguments")})
+            return Err(OptError {
+                cause: format!("the option {name} can't be set after parsing arguments"),
+            });
         }
         for opt in &self.opts {
             if opt.nme == name {
-                return Err(OptError{cause: format!("repeating option {name}")})
+                return Err(OptError {
+                    cause: format!("repeating option {name}"),
+                });
             }
         }
-        self.opts.push(CliOpt{ t, nme: name.to_string(), descr:None, v:None});
+        self.opts.push(CliOpt {
+            t,
+            nme: name.to_string(),
+            descr: None,
+            v: None,
+        });
         Ok(self)
     }
-    
+
     pub fn description(&mut self, descr: &str) -> &mut Self {
         match self.opts.last_mut() {
             Some(element) => element.descr = Some(descr.to_string()),
-            _ => self.descr = Some(descr.to_string())
+            _ => self.descr = Some(descr.to_string()),
         }
         self
     }
-    
+
     pub fn get_description(&self) -> Option<String> {
         let mut descr = String::new();
         if let Some(some_descr) = &self.descr {
@@ -92,74 +101,86 @@ impl CLI {
             Some(descr)
         }
     }
-    
+
     pub fn get_opt(&mut self, name: &str) -> Option<&OptVal> {
         if self.unprocessed {
             self.parse()
         }
         for opt in &self.opts {
             if opt.nme == name {
-                return opt.v.as_ref()
+                return opt.v.as_ref();
             }
         }
         None
     }
-    
+
     pub fn args(&mut self) -> &Vec<String> {
         if self.unprocessed {
             self.parse()
         }
         &self.args
     }
-    
+
     fn parse(&mut self) {
         let mut args = env::args();
         args.next(); // swallow first
         while let Some(arg) = args.next() {
-            if arg.starts_with(OPT_PREFIX) {
+            if let Some(sarg) = arg.strip_prefix(OPT_PREFIX) {
                 // TODO eat extra -'s
-                let sarg = arg.strip_prefix(OPT_PREFIX).unwrap();
                 for opt in &mut self.opts {
                     if opt.nme == sarg {
                         match opt.t {
-                            OptTyp::Num => if let Some(val) = args.next() {
+                            OptTyp::Num => {
+                                if let Some(val) = args.next() {
                                     match val.parse::<i64>() {
-                                        Ok (num) => opt.v = Some(OptVal::Num(num)),
-                                        _ => opt.v = Some(OptVal::Unmatch)
+                                        Ok(num) => opt.v = Some(OptVal::Num(num)),
+                                        _ => opt.v = Some(OptVal::Unmatch),
                                     }
                                 }
+                            }
                             OptTyp::None => opt.v = Some(OptVal::Empty),
-                            OptTyp::FNum => if let Some(val) = args.next() {
-                                match val.parse::<f64>() {
-                                    Ok (num) => opt.v = Some(OptVal::FNum(num)),
-                                    _ => opt.v = Some(OptVal::Unmatch)
+                            OptTyp::FNum => {
+                                if let Some(val) = args.next() {
+                                    match val.parse::<f64>() {
+                                        Ok(num) => opt.v = Some(OptVal::FNum(num)),
+                                        _ => opt.v = Some(OptVal::Unmatch),
+                                    }
                                 }
                             }
-                            OptTyp::Str => if let Some(str) = args.next() { opt.v = Some(OptVal::Str(str)) }
-                            OptTyp::InStr => ()
+                            OptTyp::Str => {
+                                if let Some(str) = args.next() {
+                                    opt.v = Some(OptVal::Str(str))
+                                }
+                            }
+                            OptTyp::InStr => (),
                         }
                     } else if opt.t == OptTyp::InStr && sarg.starts_with(&opt.nme) {
                         if opt.v.is_none() {
-                           opt.v = Some(OptVal::Arr(HashSet::new()))
-                        } 
+                            opt.v = Some(OptVal::Arr(HashSet::new()))
+                        }
                         match &mut opt.v {
                             &mut Some(OptVal::Arr(ref mut set)) => {
-                            
-                                if let Some(pair) = sarg.strip_prefix(&opt.nme).unwrap().split_once('=') {
+                                if let Some(pair) =
+                                    sarg.strip_prefix(&opt.nme).unwrap().split_once('=')
+                                {
                                     set.insert((pair.0.to_string(), pair.1.to_string()));
                                 } else {
-                                    set.insert((sarg.strip_prefix(&opt.nme).unwrap().to_string(), String::new()));
+                                    set.insert((
+                                        sarg.strip_prefix(&opt.nme).unwrap().to_string(),
+                                        String::new(),
+                                    ));
                                 }
                             }
                             _ => {
-                            // somehow to report data inconsistency
+                                // somehow to report data inconsistency
                                 opt.v = Some(OptVal::Arr(HashSet::new()))
                             }
                         }
-                    } else if opt.nme.len() == 1 && sarg.contains(&opt.nme) && opt.t == OptTyp::None{
-                        opt.v = Some(OptVal::Empty) ;
+                    } else if opt.nme.len() == 1 && sarg.contains(&opt.nme) && opt.t == OptTyp::None
+                    {
+                        opt.v = Some(OptVal::Empty);
                     }
-                } 
+                }
             } else {
                 self.args.push(arg)
             }
